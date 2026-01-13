58f1c54a80c2fca4a4520d8514ac973b5e12c4f7
RedBread <trefel.yt@gmail.com>
2026-01-13 16:53:13 +0100

    automatic dates in rooms

--- Data/RoomManagerDbContext.cs
+++ Data/RoomManagerDbContext.cs
@@ -6,7 +6,52 @@ namespace RoomManagerApp.Data
     public class RoomManagerDbContext : DbContext
     {
 
-        public DbSet<Room> rooms { get; set; } = null!;
+        public DbSet<Room> Rooms { get; set; } = null!;
+
+        public override int SaveChanges()
+        {
+            var now = DateTime.UtcNow;
+
+            // Przypisanie nowo utworzonym wpisom Room odpowiedni Czas Stworzenia do 
+            foreach (var entry in ChangeTracker.Entries<Room>()
+                .Where(e => e.State == EntityState.Added))
+            {
+                entry.Entity.CreatedAt = now;
+                entry.Entity.UpdatedAt = now;
+
+            }
+
+            // Przypisanie zmodyfikowanym wpisom Room odpowiedni Czas Stworzenia do UpdatedAt
+            foreach (var entry in ChangeTracker.Entries<Room>()
+                .Where(e => e.State == EntityState.Modified))
+            {
+                entry.Entity.UpdatedAt = now;
+                entry.Property(x => x.CreatedAt).IsModified = false;
+            }
+
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var now = DateTime.UtcNow;
+
+            foreach (var entry in ChangeTracker.Entries<Room>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = now;
+                    entry.Entity.UpdatedAt = now;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = now;
+                    entry.Property(x => x.CreatedAt).IsModified = false;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
 
 
         public RoomManagerDbContext(DbContextOptions<RoomManagerDbContext> options) : base(options) { }
