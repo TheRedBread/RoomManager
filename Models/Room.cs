fc5d098168e446bf8dfd36a658001ce17e137ae9
RedBread <trefel.yt@gmail.com>
2026-01-13 14:58:59 +0100

    Room Created

---
+++ Models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomManagerApp.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public int? OrganizationId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = default!;
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
